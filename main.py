d3e1f7aab4aa9cbd1c7669e14bddb4bdebbc08e1
Lorenzo Demiri <lorenzo.demiri96@gmail.com>
2021-01-22 12:07:44 +0000

    commit

--- main.py
+++ main.py
@@ -2,7 +2,9 @@ from Kmeans import Kmeans
 from sklearn.datasets.samples_generator import make_blobs
 import matplotlib.pyplot as plt 
 X, y_true = make_blobs(n_samples=300, centers=4, cluster_std=0.60, random_state=0)
-
+print(X.shape)
+plt.scatter(X[:, 0], X[:, 1], s=50)
+plt.show()
 
 kmeans = Kmeans(n_cluster=4, max_iter=100)
 kmeans.fit(X)
